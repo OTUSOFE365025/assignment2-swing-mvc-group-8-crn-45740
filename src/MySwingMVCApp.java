9bdc7c00102c8d3e92a58885fee81aa66b74462a
reyesgeral24 <reyesgeral24@gmail.com>
2025-10-08 21:08:22 -0400

    Delete src directory

--- src/MySwingMVCApp.java
+++
@@ -1,7 +0,0 @@
-public class MySwingMVCApp {
-    public static void main(String[] args) {
-        CashRegister model = new CashRegister("products.txt");
-        RegisterView view = new RegisterView();
-        RegisterController controller = new RegisterController(model, view);
-    }
-}
